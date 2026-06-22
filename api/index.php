0b0d7d5fc56c530464335922e976937e49aa3d40
Jarrz904 <mfajarsidik64@gmail.com>
2026-06-22 23:42:47 +0700

    Kompilasi aset produksi lokal dan pasang node modules

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
-// Meneruskan request ke file index.php bawaan Laravel di folder public
+
+// Mengarahkan Vercel untuk mengeksekusi file public/index.php milik Laravel
 require __DIR__ . '/../public/index.php';
+
+// Memaksa inisialisasi core service jika terlewat oleh runtime
+if (isset($app) && !$app->booted()) {
+    $app->boot();
+}
