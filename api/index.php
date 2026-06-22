d1f150ac71afd59f3c402d261fca47f159561080
Jarrz904 <mfajarsidik64@gmail.com>
2026-06-23 00:24:35 +0700

    Paksa upload database sqlite ke production

--- api/index.php
+++ api/index.php
@@ -1,15 +1,3 @@
 <?php
-
-// Pastikan file bootstrap dimuat dengan benar
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Paksa agar Laravel memuat konfigurasi dasar agar system 'view' tidak hilang
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+// Jangan ada require tambahan selain ini
+require __DIR__ . '/../public/index.php';
