df410dd529db0a5d964670c8863d62bc0ad5ce30
Jarrz904 <mfajarsidik64@gmail.com>
2026-06-23 10:36:49 +0700

    Fix target class view not found by forcing bootstrap

--- api/index.php
+++ api/index.php
@@ -1,19 +1,12 @@
 <?php
 
-// 1. Load Autoloader
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// 1. Muat autoload dari root vendor
 require __DIR__ . '/../vendor/autoload.php';
 
-// 2. Load Aplikasi
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// 3. PAKSA BOOTSTRAP (Ini kunci agar sistem 'view' terdaftar)
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-// 4. Handle Request
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-$kernel->terminate($request, $response);
+// 2. Jalankan bootstrap & handle request khas Laravel 12
+(require_once __DIR__ . '/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
